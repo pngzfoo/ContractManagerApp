db63409a69204eed01c850fe0b572453446f5bd7
pngzfoo <tanylap_sath@hotmail.com>
2023-06-16 12:10:23 +0700

    create database

---
+++ app/src/main/java/com/pngzfoo/contractmanagerapp/room/UserDatabase.kt
@@ -0,0 +1,34 @@
+package com.pngzfoo.contractmanagerapp.room
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [User::class],version = 1)
+abstract class UserDatabase : RoomDatabase(){
+
+    abstract  val userDAO: UserDAO
+
+    //singleton
+    companion object{
+
+        @Volatile // makes the field immediately made visible to other threads
+        private var INSTANCE: UserDatabase? = null
+        fun getInstance(context: Context):UserDatabase{
+            synchronized(this){
+                var instance = INSTANCE
+                if(instance == null){
+                    //creating the database object
+                    instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    UserDatabase::class.java,
+                    "user_db"
+                    ).build()
+                }
+                return instance
+            }
+        }
+    }
+
+}
